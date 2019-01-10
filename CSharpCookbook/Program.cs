029d5ea156bb2c56480edb0e05aa1cc71913da9e
Zhou Jun <Geek168@outlook.com>
2019-01-10 21:56:00 +0800

    Add project files.

---
+++ CSharpCookbook/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCookbook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test(10, "");
+            Console.ReadLine();
+        }
+    }
+
+    struct Test
+    {
+        public int Age;
+        public string Name;
+
+        public Test(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+    }
+}
